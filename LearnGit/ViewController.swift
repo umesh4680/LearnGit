521be117317cf0a9865d54d76cc02fee5bd8e83d
umesh4680 <umesh4680@gmail.com>
2023-04-17 17:19:36 +0530

    Added Second VC

--- LearnGit/ViewController.swift
+++ LearnGit/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // Added code for testing
+        // UI Setup for Navigation Bar
         self.setNavigationBarAppearance()
         
     }
